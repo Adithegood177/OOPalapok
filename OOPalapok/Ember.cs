fccf8a1910e0a4488d08ca388100042dbd111ae5
Petrik Diak <diak@petrik.hu>
2025-02-04 09:05:16 +0100

    Add project files.

---
+++ OOPalapok/Ember.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+	internal class Ember
+	{
+		private double testSuly { get; set; }
+		private double testMagassag { get; set; }
+
+		public Ember(double TestSuly, double TestMagassag)
+		{
+			testSuly = TestSuly;
+			testMagassag = TestMagassag;
+
+		}
+
+
+		public override string ToString()
+		{
+			return $"Testsúly: {testSuly}\n Testmagasság: {testMagassag}";
+		}
+
+		public double TestTomegIndex()
+		{
+			double tt = Math.Round(testSuly / Math.Pow(testMagassag,2), 2);
+
+			return tt;
+		}
+		public bool NormalTTi (double TTi)
+		{
+			if (TTi < 25 && TTi >= 18.5)
+			{
+				return true ;
+
+			}
+			else { return false; }
+		}
+	}
+}
---
+++ OOPalapok/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPalapok
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+            Console.WriteLine("Milyen magas vagy?  [m]");
+            double magassag = Math.Round(random.NextDouble()+1,2);
+            Console.WriteLine(magassag);
+            Console.WriteLine("Milyen súlyú vagy? [kg]");
+			double suly = Math.Round(random.NextDouble()*100);
+            Console.WriteLine(suly);
+            Ember decemeber = new Ember(suly,magassag);
+
+
+			
+			 if (decemeber.NormalTTi(decemeber.TestTomegIndex()) == true)
+			{
+                Console.WriteLine("Jó formába vagy");
+
+            } else { Console.WriteLine("Gatya"); }
+            Console.WriteLine(decemeber);
+        }
+	}
+}
